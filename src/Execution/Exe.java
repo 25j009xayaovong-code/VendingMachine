eea5cf6267a986ba8b6c534dce1ec039b699f0ad
25j009xayaovong <25j009_xayaovong@edu.kimura.ac.jp>
2026-02-19 22:00:08 +0900

    some more code added

--- src/Execution/Exe.java
+++ src/Execution/Exe.java
@@ -1,5 +1,28 @@
 package Execution;
 
+import Operation.CheckBalance;
+import java.util.*;
+
 public class Exe {
-    
+    public static Scanner sc = new Scanner(System.in);
+
+    public Exe() {
+
+    }
+
+    public void execute() {
+        System.out.println("Insert Money: ");
+        int money = sc.nextInt();
+        System.out.println("Insert id: ");
+        int id = sc.nextInt();
+        CheckBalance cb = new CheckBalance(money);
+        String[] list = cb.getMenu(2);
+        System.out.println("Item Name: " + list[0] + ", Item price" + list[1]);
+        if (cb.checkMoney(id)) {
+            System.out.println("You can buy it");
+        } else {
+            System.out.println("Money is not enough");
+
+        }
+    }
 }
